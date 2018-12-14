3146649152e697ec7f6ab9fa558434be67eb2f4a
Stefan Larsson <stefan@lastsys.com>
2018-12-14 19:34:38 +0100

    Added F# version of day 1.

---
+++ day_01/fsharp/Program.fs
@@ -0,0 +1,33 @@
+﻿// Learn more about F# at http://fsharp.org
+module Program
+
+open System
+
+module Seq =
+    let rec repeat items = 
+      seq { yield! items  
+            yield! repeat items }
+
+let load_file filename =
+    filename
+    |> System.IO.File.ReadLines
+    |> Seq.map int
+    |> Seq.toArray
+
+let part1 values = Seq.sum values
+
+let part2 values =
+    let scanner state value = (fst state + value, state ||> Set.add)
+    let skipCondition state = state ||> Set.contains |> not
+    Seq.repeat values
+    |> Seq.scan scanner (0, Set.empty)
+    |> Seq.skipWhile skipCondition
+    |> Seq.head
+    |> fst
+
+[<EntryPoint>]
+let main argv =
+    let values = load_file "../input.txt"
+    values |> part1 |> printfn "Part 1: %A"
+    values |> part2 |> printfn "Part 2: %A"
+    0
