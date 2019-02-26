dfd5a6b460c84d3227cda4e4afe0a9938c2cee7e
Stefan Larsson <stefan@lastsys.com>
2019-02-26 12:21:02 +0100

    Added Racket for Day 1

---
+++ day_01/racket/day01.rkt
@@ -0,0 +1,28 @@
+#lang racket
+(require math/base)
+(require racket/set)
+(require racket/generator)
+
+(define (load-data filename)
+  (map string->number (file->lines filename #:mode 'text)))
+
+(define (part1 values)
+  (printf "Part1: ~a\n" (sum values)))
+
+(define (sum-until-cycle values sum visited)
+  (let ([updated-sum (+ sum (values))])
+    (if (set-member? visited updated-sum)
+        updated-sum
+        (sum-until-cycle
+         values
+         updated-sum
+         (set-add visited updated-sum))))) 
+
+(define (part2 values)
+  (let ([repeated-values (sequence->repeated-generator values)])
+    (let ([cycle-sum (sum-until-cycle repeated-values 0 (set))])    
+      (printf "Part2: ~a\n" cycle-sum))))
+
+(let ([values (load-data "../input.txt")])
+  (part1 values)
+  (part2 values))
